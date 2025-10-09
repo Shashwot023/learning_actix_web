8ce7b61b8f66c44e8b0c6613d30dc71164228e27
Shashwot <shashwothapa@gmail.com>
2025-10-09 20:18:41 +0545

    shared_mutable_state

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-use actix_web::{App, get, web, Responder, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
+use std::sync::Mutex;
 
-struct AppState{
-    app_name: String,
+struct AppStateWithCounter {
+    counter: Mutex<i32>,
 }
 
-async fn index(data: web::Data<AppState>) -> impl Responder {
-    let app_name = &data.app_name;
-    HttpResponse::Ok().body(format!("Welcome to {app_name}"))
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+
+    format!("Request number: {counter}")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app_state = web::Data::new(AppState {
-        app_name: String::from("Math Helper"),
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
     });
 
     HttpServer::new(move || {
-                    App::new()
-                    .app_data(app_state.clone())
-                    .route("/", web::get().to(index))
-                    })
+        App::new()
+        .app_data(counter.clone())
+        .route("/", web::get().to(index))
+    })
     .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
+    
