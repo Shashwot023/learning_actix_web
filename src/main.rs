d2672cd1f60969c47d9e78672ad89c06b41d6805
Shashwot <shashwothapa@gmail.com>
2025-10-12 10:56:01 +0545

    SomeIterationTest

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,9 @@
-use actix_web::{web, App, HttpServer};
-use std::sync::Mutex;
+fn main() {
+    let v1 = vec![1,2,3];
 
-struct AppStateWithCounter {
-    counter: Mutex<i32>,
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {val}");
+    }
 }
-
-async fn index(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap();
-    *counter += 1;
-
-    format!("Request number: {counter}")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
-
-    HttpServer::new(move || {
-        App::new()
-        .app_data(counter.clone())
-        .route("/", web::get().to(index))
-    })
-    .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
-}
-    
